8e703616fae185f328f0580f6c9ce73f4605144f
SamSuffit <samuel.santschi@gmail.com>
2014-12-24 11:04:46 +0100

    First try of a rake session helpers

---
+++ session_helpers.rb
@@ -0,0 +1,9 @@
+require 'rack/session/redis'
+
+def self.configure_rake_session
+  use Rack::Session::Redis,
+    key: 'rack.session',
+    path: '/',
+    expire_after: 3600, # In seconds
+    redis_server: "redis://#{REDIS[:host]}:#{REDIS[:port]}/0/#{REDIS[:redis_root]}-rack:session"
+end
