3f612eb841453c58bd91cc8fc3a7206600949874
SamSuffit <samuel.santschi@gmail.com>
2014-12-24 11:34:02 +0100

    set configure_rake_session method static

--- lib/session_helpers.rb
+++ lib/session_helpers.rb
@@ -2,12 +2,12 @@ require 'rack/session/redis'
 
 module SessionHelpers
 
-  def configure_rake_session
+  def self.configure_rake_session
     use Rack::Session::Redis,
       key: 'rack.session',
       path: '/',
       expire_after: 3600, # In seconds
       redis_server: "redis://#{REDIS[:host]}:#{REDIS[:port]}/0/#{REDIS[:redis_root]}-rack:session"
   end
-  
+
 end
