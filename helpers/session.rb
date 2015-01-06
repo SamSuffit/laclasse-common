370df7aa56e26ca1ef3fe08925a194395c11b013
Gwenhael Le Moine <glemoine@erasme.org>
2015-01-06 17:04:16 +0100

    refactoring, organisation of modules, naming (same for authentication.rb)

--- helpers/session.rb
+++ helpers/session.rb
@@ -1,13 +1,17 @@
+# -*- encoding: utf-8 -*-
+
 require 'rack/session/redis'
 
-module SessionHelpers
-
-  def self.configure_rake_session ( rack_builder )
-    rack_builder.use Rack::Session::Redis,
-      key: 'rack.session',
-      path: '/',
-      expire_after: 3600, # In seconds
-      redis_server: "redis://#{REDIS[:host]}:#{REDIS[:port]}/0/#{REDIS[:redis_root]}-rack:session"
+module Laclasse
+  module Helpers
+    module Session
+      def self.configure_rake_session( rack_builder )
+        rack_builder.use Rack::Session::Redis,
+                         key: 'rack.session',
+                         path: '/',
+                         expire_after: 3600, # In seconds
+                         redis_server: "redis://#{REDIS[:host]}:#{REDIS[:port]}/0/#{REDIS[:redis_root]}-rack:session"
+      end
+    end
   end
-
 end
